21196c8adcc4f6dca99b6366b74811e899a4ab1b
caronic <122324550@qq.com>
2016-08-24 22:41:52 +0800

    modified synchronized example

--- jwisdom-core/src/main/java/com/caronic/jwisdom/core/exercise/concurrent/Synchronized.java
+++ jwisdom-core/src/main/java/com/caronic/jwisdom/core/exercise/concurrent/Synchronized.java
@@ -8,29 +8,106 @@ import java.util.concurrent.TimeUnit;
 public class Synchronized {
     public static void main(String[] args) {
         Synchronized o = new Synchronized();
+        Synchronized s = new Synchronized();
         Thread threadA = new Thread(() -> {
-            synchronized (o) {
-                o.methodA();
+            // obtain object lock via synchronized method
+            o.methodA();
+        });
+
+        Thread threadB = new Thread(() -> {
+            // obtain object lock via synchronized method
+            o.methodB();
+        });
+
+        Thread threadC = new Thread(() -> {
+            // obtain the object lock via synchronized block
+            synchronized (s) {
+                s.methodC();
                 try {
                     TimeUnit.SECONDS.sleep(5);
                 } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         });
 
-        Thread threadB = new Thread(() -> {
-            o.methodB();
+        Thread threadD = new Thread(() -> {
+            // obtain the object lock via synchronized block
+            synchronized (s) {
+                s.methodD();
+            }
+        });
+
+        Thread threadE = new Thread(() -> {
+            // obtain the Class lock via synchronized static method
+            synchronized (Synchronized.class) {
+                Synchronized.methodE();
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread threadF = new Thread(() -> {
+            // obtain the Class lock via synchronized static block
+            synchronized (Synchronized.class) {
+                Synchronized.methodF();
+            }
         });
 
         threadA.start();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         threadB.start();
+
+        threadC.start();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        threadD.start();
+
+        threadE.start();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        threadF.start();
     }
 
     public synchronized void methodA() {
         System.out.println("Enter into method A");
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public synchronized void methodB() {
         System.out.println("Enter into method B");
+    }
+
+    public void methodC() {
+        System.out.println("Enter into method C");
+    }
+
+    public void methodD() {
+        System.out.println("Enter into method D");
+    }
+
+    public synchronized static void methodE() {
+        System.out.println("Enter into method E");
+    }
+
+    public synchronized static void methodF() {
+        System.out.println("Enter into method F");
     }
 }
